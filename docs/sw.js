44475e18fb51c329099c584d1ee99ba5893f123e
austin <eliottroussille@gmail.com>
2025-06-09 19:04:27 +0200

    🌟: online page

--- docs/sw.js
+++ docs/sw.js
@@ -32,22 +32,41 @@ if (!self.define) {
     };
 }
 
-define(["./workbox-915e8d08"], (function (e) {
+define(["./workbox-915e8d08"], (function (workbox) {
     "use strict";
 
     self.addEventListener("message", (e => { e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting() }));
 
-    e.precacheAndRoute(
+    workbox.precacheAndRoute(
         [
             { url: "index.html", revision: "98b61df3f16693c28582ff59a036ff4a" },
             { url: "manifest.json", revision: "980187c41fe9f2687894ab9ed671a638" },
             { url: "widget.html", revision: "2dabfb9a8f5179c11f000898d6fb54c3" },
-            { url: "widget.js", revision: "d48d7e124f19940f958d12af7b8519c0" }
+            { url: "widget.js", revision: "d48d7e124f19940f958d12af7b8519c0" },
+            { url: "offline.html", revision: null }
         ],
         {
             ignoreURLParametersMatching: [/^utm_/, /^fbclid$/]
         }
     );
+
+    workbox.routing.registerRoute(
+    // Cible toutes les requêtes de navigation (pages HTML)
+    ({ request }) => request.mode === 'navigate',
+    // Strategy: essaie le réseau, sinon renvoie le cache 'offline.html'
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'pages-cache',
+        plugins: [
+        {
+            cacheWillUpdate: async ({ response }) => {
+            // on ne met en cache que les 200 OK
+            return response && response.status === 200 ? response : null;
+            }
+        }
+        ]
+    })
+    );
+
 
     // =========================
     // Widget Windows 11 events
@@ -99,23 +118,3 @@ define(["./workbox-915e8d08"], (function (e) {
     });
 }));
 //# sourceMappingURL=sw.js.map
-
-// self.addEventListener("push", (event) => {
-//     const data = event.data ? event.data.json() : {};
-//     const title = data.title || "TaskBoard Notification";
-//     const options = {
-//         body: data.body || "",
-//         icon: "assets/icons/icon-192.png"
-//     };
-//     event.waitUntil(self.registration.showNotification(title, options));
-// });
-// self.addEventListener("sync", (event) => {
-//     if (event.tag === "sync-tasks") {
-//         event.waitUntil(fetch("/api/widget-data").catch(() => {}));
-//     }
-// });
-// self.addEventListener("periodicsync", (event) => {
-//     if (event.tag === "update-tasks") {
-//         event.waitUntil(fetch("/api/widget-data").catch(() => {}));
-//     }
-// });
